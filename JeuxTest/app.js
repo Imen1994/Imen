973120255980b9b3068f784df057bcd76af90052
Imen1994 <imen.boufares@supcom.tn>
2017-04-09 12:35:35 +0200

    Add files via upload

---
+++ JeuxTest/app.js
@@ -0,0 +1,30 @@
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
+var app = express();
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+var io = require ('socket.io').listen(app.listen(appEnv.port, '0.0.0.0',function() {
+	console.log("server starting on" + appEnv.url);
+}
+
+	));
+
+require('./config')(app, io);
+require('./routes')(app, io);
